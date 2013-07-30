f6659e95d463b6ea5aeaa63c05f94367517c5929
Patrick Mooney <patmooney@gmail.com>
2013-07-30 12:34:38 +0100

    Added password and confirm password fields to logged out

--- prototype/Logged_out_files/axurerp_pagespecificscript.js
+++ prototype/Logged_out_files/axurerp_pagespecificscript.js
@@ -1,10 +1,114 @@
-﻿for(var i = 0; i < 96; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 112; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-gv_vAlignTable['u81'] = 'center';gv_vAlignTable['u83'] = 'center';gv_vAlignTable['u85'] = 'center';gv_vAlignTable['u86'] = 'top';gv_vAlignTable['u89'] = 'center';gv_vAlignTable['u90'] = 'top';gv_vAlignTable['u91'] = 'top';gv_vAlignTable['u92'] = 'top';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u24'] = 'center';gv_vAlignTable['u28'] = 'center';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u34'] = 'center';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u93'] = 'top';gv_vAlignTable['u94'] = 'top';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u42'] = 'center';
+gv_vAlignTable['u81'] = 'center';gv_vAlignTable['u82'] = 'top';gv_vAlignTable['u85'] = 'center';gv_vAlignTable['u87'] = 'center';gv_vAlignTable['u89'] = 'top';gv_vAlignTable['u90'] = 'top';
+u91.style.cursor = 'pointer';
+$axure.eventManager.click('u91', function(e) {
+
+if ((GetFocusedWidgetText()) == ('')) {
+
+	SetPanelState('u88', 'pd1u88','none','',500,'none','',500);
+
+	SetPanelState('u83', 'pd1u83','none','',500,'none','',500);
+
+}
+else
+if (true) {
+
+	SetPanelState('u83', 'pd1u83','none','',500,'none','',500);
+
+	SetPanelState('u88', 'pd2u88','none','',500,'none','',500);
+
+}
+});
+
+$axure.eventManager.keyup('u91', function(e) {
+
+if ((GetWidgetText('u91')) != ('')) {
+
+	SetPanelState('u88', 'pd2u88','none','',500,'none','',500);
+
+}
+else
+if (true) {
+
+	SetPanelState('u88', 'pd0u88','none','',500,'none','',500);
+
+}
+});
+
+$axure.eventManager.blur('u91', function(e) {
+
+if (true) {
+
+	SetPanelState('u83', 'pd0u83','none','',500,'none','',500);
+
+}
+else
+if ((GetWidgetText('u91')) == ('""')) {
+
+	SetPanelState('u83', 'pd0u83','none','',500,'none','',500);
+
+	SetPanelState('u88', 'pd0u88','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u110'] = 'top';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u24'] = 'center';gv_vAlignTable['u28'] = 'center';gv_vAlignTable['u100'] = 'top';
+u101.style.cursor = 'pointer';
+$axure.eventManager.click('u101', function(e) {
+
+if ((GetFocusedWidgetText()) == ('')) {
+
+	SetPanelState('u98', 'pd1u98','none','',500,'none','',500);
+
+	SetPanelState('u93', 'pd1u93','none','',500,'none','',500);
+
+}
+else
+if (true) {
+
+	SetPanelState('u93', 'pd1u93','none','',500,'none','',500);
+
+	SetPanelState('u98', 'pd2u98','none','',500,'none','',500);
+
+}
+});
+
+$axure.eventManager.keyup('u101', function(e) {
+
+if ((GetWidgetText('u101')) != ('')) {
+
+	SetPanelState('u98', 'pd2u98','none','',500,'none','',500);
+
+}
+else
+if (true) {
+
+	SetPanelState('u98', 'pd0u98','none','',500,'none','',500);
+
+}
+});
+
+$axure.eventManager.blur('u101', function(e) {
+
+if (true) {
+
+	SetPanelState('u93', 'pd0u93','none','',500,'none','',500);
+
+}
+else
+if ((GetWidgetText('u101')) == ('""')) {
+
+	SetPanelState('u93', 'pd0u93','none','',500,'none','',500);
+
+	SetPanelState('u98', 'pd0u98','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u105'] = 'center';gv_vAlignTable['u106'] = 'top';gv_vAlignTable['u107'] = 'top';gv_vAlignTable['u108'] = 'top';gv_vAlignTable['u109'] = 'top';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u34'] = 'center';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u95'] = 'center';gv_vAlignTable['u97'] = 'center';gv_vAlignTable['u99'] = 'top';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u42'] = 'center';
 $axure.eventManager.mouseover('u44', function(e) {
 if (!IsTrueMouseOver('u44',e)) return;
 if (true) {
