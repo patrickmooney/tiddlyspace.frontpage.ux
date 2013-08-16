b22abcd6e9a23f9dc1b5d27220ec85565307f399
Patrick Mooney <patmooney@gmail.com>
2013-08-16 11:14:12 +0100

    Updated the activity stream

--- prototype/Tiddler_Feed_files/axurerp_pagespecificscript.js
+++ prototype/Tiddler_Feed_files/axurerp_pagespecificscript.js
@@ -1,61 +1,230 @@
-﻿for(var i = 0; i < 56; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 155; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
-});
-gv_vAlignTable['u50'] = 'center';u54.tabIndex = 0;
-
-u54.style.cursor = 'pointer';
-$axure.eventManager.click('u54', function(e) {
-
 if (true) {
 
-	SetPanelVisibility('u53','hidden','none',500);
+	SetPanelState('u3', 'pd1u3','none','',500,'none','',500);
+
+}
+
+});
+
+widgetIdToPanelStateChangeFunction['u3'] = function() {
+var e = windowEvent;
+
+if ((GetPanelState('u3')) == ('pd1u3')) {
+
+	MoveWidgetTo('u112', GetNum('328'), GetNum('0'),'none',500);
+
+	MoveWidgetTo('u122', GetNum('328'), GetNum('0'),'none',500);
+
+	MoveWidgetTo('u134', GetNum('328'), GetNum('0'),'none',500);
+
+	MoveWidgetTo('u144', GetNum('328'), GetNum('0'),'none',500);
 
 SetWidgetRichText('u7', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">1 minute ago</span></p>');
 
-SetWidgetRichText('u17', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">25 minutes ago</span></p>');
+SetWidgetRichText('u17', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">2 minutes ago</span></p>');
 
-SetWidgetRichText('u27', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">35 minutes ago</span></p>');
+SetWidgetRichText('u12', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">3 minutes ago</span></p>');
+
+SetWidgetRichText('u34', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">4 minutes ago</span></p>');
+
+SetWidgetRichText('u44', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">5 minutes ago</span></p>');
+
+SetWidgetRichText('u39', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">6 minutes ago</span></p>');
+
+	MoveWidgetBy('u3', GetNum('0'), GetNum('60'),'linear',500);
+
+	SetPanelVisibility('u112','','fade',500);
+
+	MoveWidgetTo('u112', GetNum('0'), GetNum('0'),'linear',500);
+function waitu370dd81d73aa4b0bb4148f1d262e372c1() {
+
+SetWidgetRichText('u7', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">2 minutes ago</span></p>');
+
+SetWidgetRichText('u17', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">3 minutes ago</span></p>');
+
+SetWidgetRichText('u12', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">4 minutes ago</span></p>');
+
+SetWidgetRichText('u34', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">5 minutes ago</span></p>');
+
+SetWidgetRichText('u44', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">6 minutes ago</span></p>');
+
+SetWidgetRichText('u39', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">7 minutes ago</span></p>');
+
+SetWidgetRichText('u116', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">1 minute ago</span></p>');
+
+	MoveWidgetBy('u3', GetNum('0'), GetNum('65'),'linear',500);
+
+	MoveWidgetBy('u112', GetNum('0'), GetNum('65'),'linear',500);
+
+	SetPanelVisibility('u122','','fade',500);
+
+	MoveWidgetTo('u122', GetNum('0'), GetNum('0'),'linear',500);
+function waitub0832e713ed448f5a9eb44f718b8ce6c1() {
+
+SetWidgetRichText('u7', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">3 minutes ago</span></p>');
+
+SetWidgetRichText('u17', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">4 minutes ago</span></p>');
+
+SetWidgetRichText('u12', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">5 minutes ago</span></p>');
+
+SetWidgetRichText('u34', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">6 minutes ago</span></p>');
+
+SetWidgetRichText('u44', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">7 minutes ago</span></p>');
+
+SetWidgetRichText('u39', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">8 minutes ago</span></p>');
+
+SetWidgetRichText('u116', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">2 minutes ago</span></p>');
+
+SetWidgetRichText('u126', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">1 minute ago</span></p>');
+
+	MoveWidgetBy('u3', GetNum('0'), GetNum('60'),'linear',500);
+
+	MoveWidgetBy('u112', GetNum('0'), GetNum('60'),'linear',500);
+
+	MoveWidgetBy('u122', GetNum('0'), GetNum('60'),'linear',500);
+
+	SetPanelVisibility('u134','','fade',500);
+
+	MoveWidgetTo('u134', GetNum('0'), GetNum('0'),'linear',500);
+function waitu68f0f7542dfc436fb698acd6bac60bb91() {
+
+SetWidgetRichText('u7', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">4 minutes ago</span></p>');
+
+SetWidgetRichText('u17', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">5 minutes ago</span></p>');
+
+SetWidgetRichText('u12', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">6 minutes ago</span></p>');
+
+SetWidgetRichText('u34', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">7 minutes ago</span></p>');
+
+SetWidgetRichText('u44', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">8 minutes ago</span></p>');
+
+SetWidgetRichText('u39', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">9 minutes ago</span></p>');
+
+SetWidgetRichText('u116', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">3 minutes ago</span></p>');
+
+SetWidgetRichText('u126', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">2 minutes ago</span></p>');
+
+SetWidgetRichText('u138', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">1 minute ago</span></p>');
+
+	MoveWidgetBy('u3', GetNum('0'), GetNum('65'),'linear',500);
+
+	MoveWidgetBy('u112', GetNum('0'), GetNum('65'),'linear',500);
+
+	MoveWidgetBy('u122', GetNum('0'), GetNum('65'),'linear',500);
+
+	MoveWidgetBy('u134', GetNum('0'), GetNum('65'),'linear',500);
+
+	SetPanelVisibility('u144','','fade',500);
+
+	MoveWidgetTo('u144', GetNum('0'), GetNum('0'),'linear',500);
+function waitu660e9672bf15493ba853abbaba3d3bc51() {
+
+SetWidgetRichText('u7', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">5 minutes ago</span></p>');
+
+SetWidgetRichText('u17', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">6 minutes ago</span></p>');
+
+SetWidgetRichText('u12', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">7 minutes ago</span></p>');
+
+SetWidgetRichText('u34', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">8 minutes ago</span></p>');
+
+SetWidgetRichText('u44', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">9 minutes ago</span></p>');
+
+SetWidgetRichText('u39', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">10 minutes ago</span></p>');
+
+SetWidgetRichText('u116', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">4 minutes ago</span></p>');
+
+SetWidgetRichText('u126', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">3 minutes ago</span></p>');
+
+SetWidgetRichText('u138', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">2 minutes ago</span></p>');
+
+SetWidgetRichText('u148', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">1 minute ago</span></p>');
+
+	SetPanelVisibility('u112','hidden','none',500);
+
+	SetPanelVisibility('u122','hidden','none',500);
+
+	SetPanelVisibility('u134','hidden','none',500);
+
+	SetPanelVisibility('u144','hidden','none',500);
+
+	MoveWidgetTo('u3', GetNum('0'), GetNum('0'),'none',500);
+
+	SetPanelState('u3', 'pd0u3','none','',500,'none','',500);
+
+	SetPanelState('u3', 'pd1u3','none','',500,'none','',500);
+
+SetWidgetRichText('u116', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">seconds ago</span></p>');
+
+SetWidgetRichText('u126', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">seconds ago</span></p>');
+
+SetWidgetRichText('u138', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">seconds ago</span></p>');
+
+SetWidgetRichText('u148', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">seconds ago</span></p>');
+}
+setTimeout(waitu660e9672bf15493ba853abbaba3d3bc51, 5000);
+}
+setTimeout(waitu68f0f7542dfc436fb698acd6bac60bb91, 5000);
+}
+setTimeout(waitub0832e713ed448f5a9eb44f718b8ce6c1, 5000);
+}
+setTimeout(waitu370dd81d73aa4b0bb4148f1d262e372c1, 5000);
+
+}
+
+}
+
+function rdo0MoveItems(e) {
+
+}
+gv_vAlignTable['u80'] = 'center';gv_vAlignTable['u82'] = 'center';gv_vAlignTable['u84'] = 'center';gv_vAlignTable['u85'] = 'top';gv_vAlignTable['u86'] = 'top';gv_vAlignTable['u88'] = 'top';gv_vAlignTable['u91'] = 'top';gv_vAlignTable['u92'] = 'top';gv_vAlignTable['u93'] = 'top';gv_vAlignTable['u96'] = 'top';gv_vAlignTable['u97'] = 'top';gv_vAlignTable['u98'] = 'top';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u131'] = 'center';gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u24'] = 'center';gv_vAlignTable['u26'] = 'center';gv_vAlignTable['u28'] = 'center';gv_vAlignTable['u101'] = 'center';gv_vAlignTable['u103'] = 'center';gv_vAlignTable['u105'] = 'center';gv_vAlignTable['u107'] = 'center';gv_vAlignTable['u109'] = 'center';gv_vAlignTable['u129'] = 'center';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u31'] = 'top';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u39'] = 'top';gv_vAlignTable['u111'] = 'center';gv_vAlignTable['u114'] = 'top';gv_vAlignTable['u115'] = 'top';gv_vAlignTable['u116'] = 'top';gv_vAlignTable['u119'] = 'center';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u43'] = 'top';gv_vAlignTable['u44'] = 'top';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u49'] = 'center';gv_vAlignTable['u121'] = 'center';gv_vAlignTable['u124'] = 'top';gv_vAlignTable['u125'] = 'top';gv_vAlignTable['u126'] = 'top';gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u51'] = 'center';gv_vAlignTable['u53'] = 'center';gv_vAlignTable['u55'] = 'center';gv_vAlignTable['u32'] = 'top';gv_vAlignTable['u58'] = 'top';gv_vAlignTable['u59'] = 'top';gv_vAlignTable['u37'] = 'top';gv_vAlignTable['u38'] = 'top';u133.tabIndex = 0;
+
+u133.style.cursor = 'pointer';
+$axure.eventManager.click('u133', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u132','hidden','none',500);
+
+;
+
+;
+
+;
 
 	MoveWidgetBy('u3', GetNum('0'), GetNum('66'),'linear',500);
 
-	MoveWidgetBy('u13', GetNum('0'), GetNum('66'),'linear',500);
+	SetPanelVisibility('u112','','fade',500);
 
-	MoveWidgetBy('u23', GetNum('0'), GetNum('66'),'linear',500);
-
-	SetPanelVisibility('u33','','fade',500);
-
-	MoveWidgetBy('u33', GetNum('-322'), GetNum('0'),'linear',500);
+	MoveWidgetBy('u112', GetNum('-322'), GetNum('0'),'linear',500);
 function waituabcd7dbbb0bb4d15ae2b1664f4fbbe391() {
 
 	MoveWidgetBy('u3', GetNum('0'), GetNum('72'),'linear',500);
 
-	MoveWidgetBy('u13', GetNum('0'), GetNum('72'),'linear',500);
+	MoveWidgetBy('u112', GetNum('0'), GetNum('72'),'linear',500);
 
-	MoveWidgetBy('u23', GetNum('0'), GetNum('72'),'linear',500);
+	MoveWidgetBy('u122', GetNum('-328'), GetNum('0'),'none',500);
 
-	MoveWidgetBy('u33', GetNum('0'), GetNum('72'),'linear',500);
+;
 
-	MoveWidgetBy('u43', GetNum('-328'), GetNum('0'),'none',500);
+;
 
-SetWidgetRichText('u7', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">5 minutes ago</span></p>');
+;
 
-SetWidgetRichText('u17', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">30 minutes ago</span></p>');
+;
 
-SetWidgetRichText('u27', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">40 minutes ago</span></p>');
+	SetPanelVisibility('u122','','fade',500);
 
-SetWidgetRichText('u37', '<p style="text-align:center;"><span style="font-family:Arial;font-size:11px;font-weight:normal;font-style:normal;text-decoration:none;color:#666666;">1 minute ago</span></p>');
+	MoveWidgetBy('u122', GetNum('-324'), GetNum('0'),'linear',500);
 
-	SetPanelVisibility('u43','','fade',500);
-
-	MoveWidgetBy('u43', GetNum('-324'), GetNum('0'),'linear',500);
-
-	SetPanelVisibility('u53','hidden','none',500);
+	SetPanelVisibility('u132','hidden','none',500);
 }
 setTimeout(waituabcd7dbbb0bb4d15ae2b1664f4fbbe391, 5000);
 
 }
 });
-gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u35'] = 'top';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u37'] = 'top';gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u17'] = 'top';
+gv_vAlignTable['u136'] = 'top';gv_vAlignTable['u137'] = 'top';gv_vAlignTable['u138'] = 'top';gv_vAlignTable['u151'] = 'center';gv_vAlignTable['u61'] = 'top';gv_vAlignTable['u64'] = 'top';gv_vAlignTable['u65'] = 'top';gv_vAlignTable['u66'] = 'top';gv_vAlignTable['u69'] = 'top';gv_vAlignTable['u141'] = 'center';gv_vAlignTable['u143'] = 'center';gv_vAlignTable['u146'] = 'top';gv_vAlignTable['u147'] = 'top';gv_vAlignTable['u148'] = 'top';gv_vAlignTable['u70'] = 'top';gv_vAlignTable['u71'] = 'top';gv_vAlignTable['u74'] = 'center';gv_vAlignTable['u76'] = 'center';gv_vAlignTable['u78'] = 'center';gv_vAlignTable['u34'] = 'top';gv_vAlignTable['u153'] = 'center';
