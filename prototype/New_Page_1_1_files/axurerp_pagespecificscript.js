8d4dc87d05eef0b585d725205e60fbb8ee8caafb
Patrick Mooney <patmooney@gmail.com>
2013-08-23 12:34:00 +0100

    Added prototype

--- prototype/New_Page_1_1_files/axurerp_pagespecificscript.js
+++ prototype/New_Page_1_1_files/axurerp_pagespecificscript.js
@@ -1,7 +1,7 @@
-﻿for(var i = 0; i < 8; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 12; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u6'] = 'top';
+gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';
